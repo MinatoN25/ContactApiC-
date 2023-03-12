b5b38d2ad0f4eeba734d5e44691ba3169c565cdf
MinatoN25 <106010357+MinatoN25@users.noreply.github.com>
2023-03-12 18:55:30 +0530

    Update AddContactRequest.cs

--- ContactsAPI/ContactsAPI/Models/AddContactRequest.cs
+++ ContactsAPI/ContactsAPI/Models/AddContactRequest.cs
@@ -6,6 +6,6 @@
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
-        public int NewField { get; set; };
+        public int NewField { get; set; }
     }
 }
